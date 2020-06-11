5017bb15c4283b6d70fd393dc577e2199faf5115
kamilGit <hantsky123@gmail.com>
2020-06-11 22:35:51 +0200

    Added: password-tooltip; password-visibility-controller; loader;

--- src/js/index.js
+++ src/js/index.js
@@ -19,13 +19,25 @@ buttonClosePopupForm.addEventListener('click', () => {
   popup.classList.add('hidden');
 })
 
+// Create loader
+const renderLoader = parent => {
+  const loader = `
+    <i class="fas fa-spinner loader"></i>
+  `
+  parent.insertAdjacentHTML('beforeend', loader);
+}
+
+let validPassword = false;
+
 // Form listener
 form.addEventListener('submit', e => {
   e.preventDefault();
   
   // Form validation
+  if (validPassword === false) return
 
   // Render loader to the DOM
+  renderLoader(form);
   // Display success message after 3 seconds
   setTimeout(() => {
     buttonShowPopupForm.classList.add('hidden');
@@ -34,7 +46,24 @@ form.addEventListener('submit', e => {
   }, 3000)
 })
 
+// Check if password is 8 to 15 characters which contain one uppercase letter, one numeric digit, and one special character
+password.addEventListener('keyup', () => {
+  const pattern = /^(?=.*\d)(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+  if (password.value.match(pattern)) {
+    password.classList.remove('invalid');
+    validPassword = true;
+  } else {
+    password.classList.add('invalid');
+    validPassword = false;
+  }
+})
 
+// Make password visible on click
+passwordVisibility.addEventListener('click', () => {
+  // Change type of the password
+  const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+  password.setAttribute('type', type);
 
-
-
+  // Toggle the icon
+  passwordVisibility.classList.toggle('fa-eye-slash');
+})
