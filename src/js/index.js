2d98d0b66580d14ba01d3fd9812a18f76a33d6b4
kamilGit <hantsky123@gmail.com>
2020-06-11 22:19:25 +0200

    Task 1

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,26 @@
-/* Here goes your JS code */
+// Elements
+const popup = document.querySelector('.popup');
+const form = document.querySelector('.login-form');
+const email = document.querySelector('#login-form__email');
+const password = document.querySelector('#login-form__password');
+const passwordVisibility = document.querySelector('.password-visibility');
+
+const buttonShowPopupForm = document.querySelector('#show-popup-form');
+const buttonClosePopupForm = document.querySelector('.close-popup-form');
+const successMessage = document.querySelector('.success-message');
+
+// Show popup form
+buttonShowPopupForm.addEventListener('click', () => {
+  popup.classList.remove('hidden');
+})
+
+// Close popup form
+buttonClosePopupForm.addEventListener('click', () => {
+  popup.classList.add('hidden');
+})
+
+
+
+
+
+
